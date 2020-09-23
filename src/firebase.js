2a9bcc092832b21c4f98057d51733ccde52c811f
bouchentouf hatim <hatimbo28@gmail.com>
2020-09-23 20:47:46 +0200

    deploy app

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgxKddCJ6DhJ7Xk6G3gwd5xiVKwKQZDLk",
-  authDomain: "clone-aws.firebaseapp.com",
-  databaseURL: "https://clone-aws.firebaseio.com",
-  projectId: "clone-aws",
-  storageBucket: "clone-aws.appspot.com",
-  messagingSenderId: "619109055289",
-  appId: "1:619109055289:web:9b34e13d04881e4ff086e8",
-  measurementId: "G-MR4NVQKQBD",
+  //your firebaseConfig
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
